fe1d57dcc4557f1e9db9ef1fe0258ad67ab3f459
Joshua Tenorio <joshliam09@gmail.com>
2023-03-29 18:09:00 -0700

    brakes

--- front-io/src/main.cpp
+++ front-io/src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
+#include <SparkFunMLX90614.h>
+
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
+
+IRTherm therm;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,6 +16,16 @@ void setup() {
   Can0.enableFIFO();
   Can0.enableFIFOInterrupt();
   Can0.mailboxStatus();
+
+  Wire.begin();
+  if (!therm.begin()) {
+    Serial.println("did not start.");
+
+    while(1); // TODO: remove when done testing
+  }
+
+  therm.setUnit(TEMP_C);
+  //therm.setEmissivity(0.55); // steel, perhaps
 
   pinMode(13, OUTPUT);
 }
@@ -28,7 +42,16 @@ void loop() {
   Serial.print("A8: ");
   Serial.print(brakePressureFrontRaw);
   Serial.print("\tA9: ");
-  Serial.println(brakePressureRearRaw);
+  Serial.print(brakePressureRearRaw);
+
+  float frBrakeTemp = 0;
+  if (therm.read()) {
+    frBrakeTemp =  therm.object();
+    Serial.print("\tAmbient: ");
+    Serial.print(therm.ambient());
+    Serial.print("\tTemp: ");
+    Serial.println(frBrakeTemp);
+  }
 
   Can0.events();
 
@@ -47,6 +70,19 @@ void loop() {
   msg.buf[4] = (brakePressureRearRaw & 0xFF00) >> 8;
   msg.buf[5] = brakePressureRearRaw & 0xFF;
   Can0.write(msg);
+
+  CAN_message_t msg1;
+  msg1.id = 0x364;
+
+  // fr brake temp
+  /*
+  msg1.buf[0] = (frBrakeTemp & 0xFF000000) >> 24;
+  msg1.buf[1] = (frBrakeTemp & 0x00FF0000) >> 16;
+  msg1.buf[2] = (frBrakeTemp & 0x0000FF00) >> 8;
+  msg1.buf[3] = (frBrakeTemp & 0x000000FF);
+  */
+
+  Can0.write(msg1);
 
   delay(20);
 }
